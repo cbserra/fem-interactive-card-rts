f2d014b2748723aa159f42901f0d937722e5fc33
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-08-14 23:03:39 -0700

    :lipstick: (front-card) align card elements with figma

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReactComponent as CardLogo } from '../assets/card-logo.svg'
+import cardLogo, { ReactComponent as CardLogo } from '../assets/card-logo.svg'
 import { CardForm } from '../types/Types'
 
 const Header = (props: { card: CardForm | undefined }) => {
@@ -11,8 +11,8 @@ const Header = (props: { card: CardForm | undefined }) => {
         </div>
         <div className="card-front">
           <div className="card-front--logo">
-            <CardLogo />
-            {/* <img src="../assets/card-logo.svg" alt="Card logo" /> */}
+            {/* <CardLogo /> */}
+            <img src={cardLogo} alt="Card logo" />
             {/* <svg width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg">
               <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/>
               <path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/>
