de6b15869a2ac52539b6439532b1d099d2bbdfce
Chris Serra <789723+cbserra@users.noreply.github.com>
2022-08-16 10:23:06 -0700

    :recycle: (css) cleaned up commented-out code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
